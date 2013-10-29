3106baa3d35eddc6df76f8b71720e9bff78f9e83
Brian Staruk <bstaruk@ampagency.com>
2013-10-29 11:17:55 -0400

    keeping things classy

--- ajax.php
+++ ajax.php
@@ -1,23 +1,32 @@
 <?php
-//Put form elements into post variables (this is where you'd sanitize your data)
-$field1 = $_POST['field1'];
+class ajaxValidate {
 
-//Establish values that will be returned via ajax
-$return = array();
-$return['msg'] = '';
-$return['error'] = false;
+	function formValidate() {
+		//Put form elements into post variables (this is where you would sanitize your data)
+		$field1 = @$_POST['field1'];
 
-//Begin form validation functionality
-if (!isset($field1) | empty($field1)){
-	$return['error'] = true;
-	$return['msg'] .= '<li>Error: Field1 is empty.</li>';
+		//Establish values that will be returned via ajax
+		$return = array();
+		$return['msg'] = '';
+		$return['error'] = false;
+
+		//Begin form validation functionality
+		if (!isset($field1) || empty($field1)){
+			$return['error'] = true;
+			$return['msg'] .= '<li>Error: Field1 is empty.</li>';
+		}
+
+		//Begin form success functionality
+		if ($return['error'] === false){
+			$return['msg'] = '<li>SUCCESS!</li>';
+		}
+
+		//Return json encoded results
+		return json_encode($return);
+	}
+
 }
 
-//Begin form success functionality
-if ($return['error'] === false){
-	$return['msg'] = '<li>SUCCESS!</li>';
-}
-
-//Return results
-echo json_encode($return);
+$ajaxValidate = new ajaxValidate;
+echo $ajaxValidate->formValidate();
 ?>
